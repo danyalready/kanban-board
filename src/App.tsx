fdbdea93bc82dfb91ae3f5a9591f493ccc09075d
Doniyor <doniyorgit@gmail.com>
2025-04-19 12:15:49 +0500

    feat: add board router

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
 import { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import type { Column, Task } from "@/db/types";
-import Kanban from "@/components/Kanban";
+import type { Board, Column, Comment, Task } from "./db/types";
+import KanbanProvider from "./contexts/KanbanProvider";
+import BoardPage from "./pages/BoardPage";
 
 export default function App() {
+    const [boards, setBoards] = useState<Board[]>([]);
     const [columns, setColumns] = useState<Column[]>([]);
     const [tasks, setTasks] = useState<Task[]>([]);
+    const [comments, setComments] = useState<Comment[]>([]);
 
     useEffect(() => {
+        setBoards([]);
         setColumns([]);
         setTasks([]);
+        setComments([]);
     }, []);
 
     return (
-        <Kanban
-            columns={columns}
-            tasks={tasks}
-            onAddColumn={() => {}}
-            onEditColumn={() => {}}
-            onDeleteColumn={() => {}}
-            onAddTask={() => {}}
-            onEditTask={() => {}}
-            onDeleteTask={() => {}}
-            onClickTask={() => {}}
-            onMoveColumn={() => {}}
-            onMoveTask={() => {}}
-        />
+        <BrowserRouter>
+            <KanbanProvider boards={boards} columns={columns} tasks={tasks} comments={comments}>
+                <Routes>
+                    <Route path="/:boardId" element={<BoardPage />} />
+                </Routes>
+            </KanbanProvider>
+        </BrowserRouter>
     );
 }
