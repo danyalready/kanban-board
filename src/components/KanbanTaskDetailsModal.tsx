c32a8ff80428effd6f0842678422e93eb28c09e4
Doniyor <doniyorgit@gmail.com>
2026-03-29 12:52:04 +0500

    feat: task description update ui

--- src/components/KanbanTaskDetailsModal.tsx
+++ src/components/KanbanTaskDetailsModal.tsx
@@ -1,6 +1,11 @@
-import type { Task } from "@/db/types";
+import { useEffect, useState } from "react";
 
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "./ui/dialog";
+import type { Task } from "@/db/types";
+import { useDebounce } from "@/hooks/useDebounce";
+import { useKanbanActions } from "@/contexts/useKanbanActions";
+
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "./ui/dialog";
+import RichTextEditor from "./RichTextEditor";
 
 interface Props {
     task: null | Task;
@@ -8,14 +13,35 @@ interface Props {
 }
 
 export default function KanbanTaskDetailsModal(props: Props) {
+    const [description, setDescription] = useState(props.task?.description || "");
+    const debouncedDescription = useDebounce(description);
+    const { updateTask } = useKanbanActions();
+
+    useEffect(() => {
+        if (!props.task || debouncedDescription === props.task.description) return;
+
+        updateTask(props.task.id, { description: debouncedDescription });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [debouncedDescription, updateTask]);
+
+    useEffect(() => {
+        if (!props.task) return;
+
+        setDescription(props.task.description);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [props.task?.id]);
+
     return (
         <Dialog open={Boolean(props.task)} onOpenChange={props.onClose}>
             <DialogContent>
                 <DialogHeader>
                     <DialogTitle>{props.task?.title}</DialogTitle>
                 </DialogHeader>
-                <DialogDescription>{props.task?.description}</DialogDescription>
-                <DialogFooter></DialogFooter>
+                <h3>Description</h3>
+                <DialogDescription>
+                    <RichTextEditor value={description} onChange={setDescription} />
+                </DialogDescription>
+                <h3>Comments</h3>
             </DialogContent>
         </Dialog>
     );
---
+++ src/components/RichTextEditor.tsx
@@ -0,0 +1,12 @@
+import ReactQuill from "react-quill";
+
+import "react-quill/dist/quill.snow.css";
+
+interface Props {
+    value: string;
+    onChange: (value: string) => void;
+}
+
+export default function RichTextEditor(props: Props) {
+    return <ReactQuill theme="snow" value={props.value} onChange={props.onChange} />;
+}
