3b465776fe8e12bdb5b09886d7f50045b318370e
Doniyor <doniyorgit@gmail.com>
2026-04-21 14:37:06 +0500

    feat: add sooner

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Toaster } from "@/components/ui/sonner";
 
 import "./index.css";
 import App from "./App";
@@ -7,5 +8,6 @@ import App from "./App";
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <App />
+        <Toaster />
     </StrictMode>,
 );
