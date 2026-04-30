783e89f10ac1e3a026a85d647a0027d1e79c1349
Doniyor <doniyorgit@gmail.com>
2026-04-30 14:39:17 +0500

    fix: redirect on 404 board

--- src/app/AppLayout.tsx
+++ src/app/AppLayout.tsx
@@ -1,15 +1,32 @@
-import { Outlet, useParams } from "react-router-dom";
+import { Outlet, useParams, Navigate, useMatch } from "react-router-dom";
 
 import Header from "@/features/kanban/components/Header";
+import { Board } from "@/domain/kanban/types";
 
 import { useKanban } from "./kanban/useKanban";
 
 export default function AppLayout() {
     const { boardId } = useParams();
+    const isBoardRoute = useMatch("/:boardId");
+
     const { state } = useKanban();
 
-    const board = state.boards.find((board) => board.id === boardId);
+    if (isBoardRoute) {
+        const board = state.boards.find((b) => b.id === boardId);
 
+        const isLoaded = state.boards.length > 0;
+
+        if (isLoaded && !board) return <Navigate to="/" replace />;
+
+        if (!board) return null;
+
+        return <Layout board={board} />;
+    }
+
+    return <Layout />;
+}
+
+function Layout({ board }: { board?: Board }) {
     return (
         <div className="flex h-screen flex-col">
             <Header board={board} />
